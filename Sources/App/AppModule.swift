33cd48db2fe3f0359a0cd85c712b678fa6931d50
Doublewater777 <return_panyang@163.com>
2026-09-03 18:31:58 +0800

    chore: restore publication argument label

--- Sources/App/AppModule.swift
+++ Sources/App/AppModule.swift
@@ -151,7 +151,7 @@ extension AppModule: ModuleDelegate {
 
 extension AppModule: LibraryModuleDelegate {
     func libraryDidSelectPublication(_ publication: Publication, book: Book) {
-        reader.presentPublication(publication: publication, book: book, in: library.rootViewController)
+        reader.presentPublication(publication, book: book, in: library.rootViewController)
     }
 }
 
@@ -182,7 +182,7 @@ extension AppModule: HomeModuleDelegate {
                         minutes: microReadingMinutes
                     )
                 }
-                reader.presentPublication(pub, book: book, in: nav)
+                reader.presentPublication(publication: pub, book: book, in: nav)
             } catch {
                 presentError(UserError(error), from: nav)
             }
