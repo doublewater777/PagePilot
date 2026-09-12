ea0cca9081542f29acef50a28eb236170e249ca7
Doublewater777 <return_panyang@163.com>
2026-09-12 13:08:03 +0800

    Cover independent Watch command and status sequences

--- Tests/WatchResponseEpochTests.swift
+++ Tests/WatchResponseEpochTests.swift
@@ -4,17 +4,17 @@ import XCTest
 final class WatchResponseEpochTests: XCTestCase {
     func testCurrentReplyIsAcceptedWhileTransportIsReachable() {
         var epoch = WatchResponseEpoch()
-        let token = epoch.beginRequest(to: .iPhone)
+        let token = epoch.beginRequest(to: .iPhone, kind: .status)
 
         XCTAssertTrue(epoch.accepts(token, transportReachable: true))
     }
 
     func testReplyFromPreviousTransportLifetimeIsRejectedAfterInvalidation() {
         var epoch = WatchResponseEpoch()
-        let staleToken = epoch.beginRequest(to: .iPhone)
+        let staleToken = epoch.beginRequest(to: .iPhone, kind: .status)
 
         epoch.invalidateTransport()
-        let currentToken = epoch.beginRequest(to: .iPhone)
+        let currentToken = epoch.beginRequest(to: .iPhone, kind: .status)
 
         XCTAssertFalse(epoch.accepts(staleToken, transportReachable: true))
         XCTAssertTrue(epoch.accepts(currentToken, transportReachable: true))
@@ -22,15 +22,15 @@ final class WatchResponseEpochTests: XCTestCase {
 
     func testCurrentReplyIsRejectedWhileTransportIsUnreachable() {
         var epoch = WatchResponseEpoch()
-        let token = epoch.beginRequest(to: .iPhone)
+        let token = epoch.beginRequest(to: .iPhone, kind: .status)
 
         XCTAssertFalse(epoch.accepts(token, transportReachable: false))
     }
 
-    func testOlderReplyForSameDestinationIsRejectedAfterNewerRequestStarts() {
+    func testOlderStatusReplyForSameDestinationIsRejectedAfterNewerStatusStarts() {
         var epoch = WatchResponseEpoch()
-        let older = epoch.beginRequest(to: .iPad)
-        let newer = epoch.beginRequest(to: .iPad)
+        let older = epoch.beginRequest(to: .iPad, kind: .status)
+        let newer = epoch.beginRequest(to: .iPad, kind: .status)
 
         XCTAssertFalse(epoch.accepts(older, transportReachable: true))
         XCTAssertTrue(epoch.accepts(newer, transportReachable: true))
@@ -38,10 +38,30 @@ final class WatchResponseEpochTests: XCTestCase {
 
     func testNewRequestForOtherDestinationDoesNotInvalidateCurrentReply() {
         var epoch = WatchResponseEpoch()
-        let iPhone = epoch.beginRequest(to: .iPhone)
-        let iPad = epoch.beginRequest(to: .iPad)
+        let iPhone = epoch.beginRequest(to: .iPhone, kind: .status)
+        let iPad = epoch.beginRequest(to: .iPad, kind: .status)
 
         XCTAssertTrue(epoch.accepts(iPhone, transportReachable: true))
         XCTAssertTrue(epoch.accepts(iPad, transportReachable: true))
+    }
+
+    func testStatusPollDoesNotInvalidateInFlightCommandForSameDestination() {
+        var epoch = WatchResponseEpoch()
+        let command = epoch.beginRequest(to: .iPad, kind: .command)
+        let status = epoch.beginRequest(to: .iPad, kind: .status)
+
+        XCTAssertTrue(epoch.accepts(command, transportReachable: true))
+        XCTAssertTrue(epoch.accepts(status, transportReachable: true))
+    }
+
+    func testNewerCommandInvalidatesOlderCommandButNotStatus() {
+        var epoch = WatchResponseEpoch()
+        let status = epoch.beginRequest(to: .iPhone, kind: .status)
+        let olderCommand = epoch.beginRequest(to: .iPhone, kind: .command)
+        let newerCommand = epoch.beginRequest(to: .iPhone, kind: .command)
+
+        XCTAssertTrue(epoch.accepts(status, transportReachable: true))
+        XCTAssertFalse(epoch.accepts(olderCommand, transportReachable: true))
+        XCTAssertTrue(epoch.accepts(newerCommand, transportReachable: true))
     }
 }
