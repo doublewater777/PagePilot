363756f33d5a74c4cc88f44adb1a919363e21e0b
Doublewater777 <return_panyang@163.com>
2026-09-12 12:53:15 +0800

    Test latest Watch response sequencing

--- Tests/WatchResponseEpochTests.swift
+++ Tests/WatchResponseEpochTests.swift
@@ -3,25 +3,45 @@ import XCTest
 
 final class WatchResponseEpochTests: XCTestCase {
     func testCurrentReplyIsAcceptedWhileTransportIsReachable() {
-        let epoch = WatchResponseEpoch()
-        let token = epoch.token
+        var epoch = WatchResponseEpoch()
+        let token = epoch.beginRequest(to: .iPhone)
 
         XCTAssertTrue(epoch.accepts(token, transportReachable: true))
     }
 
     func testReplyFromPreviousTransportLifetimeIsRejectedAfterInvalidation() {
         var epoch = WatchResponseEpoch()
-        let staleToken = epoch.token
+        let staleToken = epoch.beginRequest(to: .iPhone)
 
         epoch.invalidateTransport()
+        let currentToken = epoch.beginRequest(to: .iPhone)
 
         XCTAssertFalse(epoch.accepts(staleToken, transportReachable: true))
-        XCTAssertTrue(epoch.accepts(epoch.token, transportReachable: true))
+        XCTAssertTrue(epoch.accepts(currentToken, transportReachable: true))
     }
 
     func testCurrentReplyIsRejectedWhileTransportIsUnreachable() {
-        let epoch = WatchResponseEpoch()
+        var epoch = WatchResponseEpoch()
+        let token = epoch.beginRequest(to: .iPhone)
 
-        XCTAssertFalse(epoch.accepts(epoch.token, transportReachable: false))
+        XCTAssertFalse(epoch.accepts(token, transportReachable: false))
+    }
+
+    func testOlderReplyForSameDestinationIsRejectedAfterNewerRequestStarts() {
+        var epoch = WatchResponseEpoch()
+        let older = epoch.beginRequest(to: .iPad)
+        let newer = epoch.beginRequest(to: .iPad)
+
+        XCTAssertFalse(epoch.accepts(older, transportReachable: true))
+        XCTAssertTrue(epoch.accepts(newer, transportReachable: true))
+    }
+
+    func testNewRequestForOtherDestinationDoesNotInvalidateCurrentReply() {
+        var epoch = WatchResponseEpoch()
+        let iPhone = epoch.beginRequest(to: .iPhone)
+        let iPad = epoch.beginRequest(to: .iPad)
+
+        XCTAssertTrue(epoch.accepts(iPhone, transportReachable: true))
+        XCTAssertTrue(epoch.accepts(iPad, transportReachable: true))
     }
 }
