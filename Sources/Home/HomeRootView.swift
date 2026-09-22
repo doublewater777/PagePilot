7b4d3a416afb115a6acca355ac454f866cc2af52
Doublewater777 <return_panyang@163.com>
2026-09-22 23:51:07 +0800

    Remove custom micro-reading sheet sizing

--- Sources/Home/HomeRootView.swift
+++ Sources/Home/HomeRootView.swift
@@ -10,14 +10,6 @@ struct HomeRootView: View {
     }
 }
 
-private struct MicroReadingContentHeightKey: PreferenceKey {
-    static let defaultValue: CGFloat = 0
-
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value = max(value, nextValue())
-    }
-}
-
 struct MicroReadingStartSheet: View {
     let bookTitle: String
     @Binding var selectedMinutes: Int
@@ -25,7 +17,6 @@ struct MicroReadingStartSheet: View {
 
     @Environment(\.colorScheme) private var colorScheme
     @Environment(\.horizontalSizeClass) private var horizontalSizeClass
-    @State private var contentHeight: CGFloat = 420
 
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
@@ -112,23 +103,7 @@ struct MicroReadingStartSheet: View {
             .padding(.top, horizontalSizeClass == .regular ? 32 : 24)
             .padding(.horizontal, horizontalSizeClass == .regular ? 32 : 24)
             .padding(.bottom, 24)
-            .background {
-                GeometryReader { proxy in
-                    Color.clear.preference(
-                        key: MicroReadingContentHeightKey.self,
-                        value: proxy.size.height
-                    )
-                }
-            }
         }
         .background(AppColors.background)
-        .onPreferenceChange(MicroReadingContentHeightKey.self) { height in
-            guard height > 0 else { return }
-            let measuredHeight = ceil(height)
-            if abs(contentHeight - measuredHeight) > 0.5 {
-                contentHeight = measuredHeight
-            }
-        }
-        .presentationDetents([.height(contentHeight)])
     }
 }
